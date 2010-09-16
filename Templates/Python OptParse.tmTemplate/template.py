9beee030f2818dc9b5f931f182817979090a4ec0
Donald Ephraim Curtis <dcurtis@cs.uiowa.edu>
2010-09-15 21:29:23 -0500

    Initial commit of the bundle.

---
+++ Templates/Python OptParse.tmTemplate/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+${TM_NEW_FILE_BASENAME}.py
+
+Created by ${TM_FULLNAME} on ${TM_DATE}.
+Copyright (c) ${TM_YEAR} ${TM_FULLNAME}. 
+
+All rights reserved.
+"""
+
+import sys
+import optparse
+
+def main(argv=None):
+	parser = optparse.OptionParser()
+	(options, args) = parser.parse_args()
+
+if __name__ == "__main__":
+	sys.exit(main())
